f6c571588873556c4042f85b89afa9e164fa7397
Kareem Muhammad <karimmuhammad924@msn.com>
2024-01-27 19:57:09 +0200

    commit changes

---
+++ src/BuildingBlocks/TherapyTrack.BuildingBlocks.Domain/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace TherapyTrack.BuildingBlocks.Domain
+{
+    public interface IDomainEvent : INotification
+    {
+        Guid Id { get; }
+
+        DateTime OccurredOn { get; }
+    }
+}
